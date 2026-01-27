9d185a703afadbc663ad10c266555ba6708ac0c7
amrita-m-pillai <amritampillai06@gmail.com>
2026-01-27 23:43:27 +0530

    first commit

--- src/Cupcake.java
+++ src/Cupcake.java
@@ -1,46 +1,31 @@
 import java.util.Scanner;
+
 class Cupcake {
     public static void main(String[] args) {
-        Scanner sc= new Scanner (System.in);
-        System.out.println("Size of the array");
-        int n= sc.nextInt();
-        System.out.println("Enter the elements of the array");
-        int num[]= new int[n];
-        for (int i=0; i<n; i++)
-        {
-            num[i]=sc.nextInt();
-        }  
-        double sum=0;
-        int l=largest(num);
-        for (int i=0; i<n;i++)
-        {
-            sum= sum+ l*Math.pow(2,j);
-            
-            num= delete(num);
-            l= largest(num);
-            
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] num = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            num[i] = sc.nextInt();
         }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (num[i] < num[j]) {
+                    int temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
+            }
+        }
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += num[i] * (int)Math.pow(2, i);
+        }
+
         System.out.println(sum);
     }
-    public static int largest(int num, int n)
-    {  int l;
-          for(int i=0; i<n; i++)
-        {
-            if(num[i]>l)
-              l=num[i];
-        }
-        return l;
-    }
-    public static int delete(int num, int n, int del)
-    {
-           for(int i=0; i<n; i++)
-        {
-            if(num[i]==del)
-              num[i]=num[i+1];
-              
-        }
-        n--;
-        return num;
-    }
-        
-        }
+}
